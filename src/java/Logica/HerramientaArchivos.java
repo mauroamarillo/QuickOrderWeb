6f56dc3a273dcc334b8e2750a8893e1582b77e5c
Jean <Jean@192.168.56.101>
2015-09-10 15:25:06 -0300

    agrege una funcion para copiar

--- src/java/Logica/HerramientaArchivos.java
+++ src/java/Logica/HerramientaArchivos.java
@@ -6,20 +6,51 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 
 public abstract class HerramientaArchivos {
 
+    public static void copiarArchivo(String source, String dest) throws IOException {
+        Files.copy(new File(source).toPath(), new File(dest).toPath());
+    }
+
     public static void copiarArchivo(File source, File dest) throws IOException {
         Files.copy(source.toPath(), dest.toPath());
     }
-    
+
     /*
      Encontre otras 3 formas mas de copiar archivos
      por lo que lei capas la siguiente sirve 
      mas al momento de implementar el servidor
      por eso lo dejo aca tambien
      */
+    
+    public static void copyFile(String inicio, String fin) throws IOException {
+        File sourceFile = new File(inicio);
+        File destFile = new File(fin);
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+        FileChannel origen = null;
+        FileChannel destino = null;
+        try {
+            origen = new FileInputStream(sourceFile).getChannel();
+            destino = new FileOutputStream(destFile).getChannel();
+
+            long count = 0;
+            long size = origen.size();
+            while ((count += destino.transferFrom(origen, count, size - count)) < size);
+            
+        } finally {
+            if (origen != null) {
+                origen.close();
+            }
+            if (destino != null) {
+                destino.close();
+            }
+        }
+    }
 
     public static void copyFileUsingFileStreams(File source, File dest) throws IOException {
         InputStream input = null;
